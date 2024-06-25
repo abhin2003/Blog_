12f0f9bf5bc858bb9d9502bd4daf0fbda6c4c9df
Abhin S Krishna <unniskrishna3@gmail.com>
2024-06-25 14:01:58 +0530

    changes done

--- src/Blog_App_frontend/src/AddBlog.jsx
+++ src/Blog_App_frontend/src/AddBlog.jsx
@@ -1,9 +1,7 @@
-// AddBlog.jsx
-
 import React from 'react';
-import { Formik, Form, Field } from 'formik';
+import { Formik, Form } from 'formik';
 import { useNavigate } from 'react-router-dom';
-import { Form as BootstrapForm } from 'react-bootstrap';
+import { Form as BootstrapForm, Button, Container, Row, Col } from 'react-bootstrap';
 import { useMutation, useQueryClient } from 'react-query';
 import './AddBlog.css';
 import { initialValues, addBlogSchema } from './constants';
@@ -18,54 +16,64 @@ function AddBlog({ handleAddBlog }) {
   });
 
   return (
-    <div className="add-blog-container">
-      <h2 className="page-title">Add Blog</h2>
-      <Formik
-        initialValues={initialValues}
-        validationSchema={addBlogSchema}
-        onSubmit={async (values, { setSubmitting, resetForm }) => {
-          try {
-            await mutation.mutateAsync(values);
-            resetForm();
-            navigate('/blogs');
-          } catch (error) {
-            console.error('Error adding blog:', error);
-          } finally {
-            setSubmitting(false);
-          }
-        }}
-      >
-        {({ isSubmitting, errors, touched }) => (
-          <Form className="add-blog-form">
-            <div className="form-group">
-              <label htmlFor="title">Title:</label>
-              <Field 
-                type="text" 
-                name="title" 
-                className={`input ${touched.title && errors.title ? 'is-invalid' : ''}`} 
-              />
-              <BootstrapForm.Control.Feedback type="invalid">
-                {errors.title}
-              </BootstrapForm.Control.Feedback>
-            </div>
-            <div className="form-group">
-              <label htmlFor="description">Description:</label>
-              <Field 
-                as="textarea" 
-                name="description" 
-                className={`textarea ${touched.description && errors.description ? 'is-invalid' : ''}`} 
-              />
-              <BootstrapForm.Control.Feedback type="invalid">
-                {errors.description}
-              </BootstrapForm.Control.Feedback>
-            </div>
-            <button type="submit" disabled={isSubmitting} className="submit-button">
-              Add Blog
-            </button>
-          </Form>
-        )}
-      </Formik>
-    </div>
+    <Container className="add-blog-container">
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <h2 className="page-title">Add Blog</h2>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={addBlogSchema}
+            onSubmit={async (values, { setSubmitting, resetForm }) => {
+              try {
+                await mutation.mutateAsync(values);
+                resetForm();
+                navigate('/blogs');
+              } catch (error) {
+                console.error('Error adding blog:', error);
+              } finally {
+                setSubmitting(false);
+              }
+            }}
+          >
+            {({ isSubmitting, errors, touched, handleChange, handleBlur, values }) => (
+              <Form className="add-blog-form">
+                <BootstrapForm.Group className="mb-3" controlId="title">
+                  <BootstrapForm.Label>Title:</BootstrapForm.Label>
+                  <BootstrapForm.Control
+                    type="text"
+                    name="title"
+                    value={values.title}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={touched.title && !!errors.title}
+                  />
+                  <BootstrapForm.Control.Feedback type="invalid">
+                    {errors.title}
+                  </BootstrapForm.Control.Feedback>
+                </BootstrapForm.Group>
+                <BootstrapForm.Group className="mb-3" controlId="description">
+                  <BootstrapForm.Label>Description:</BootstrapForm.Label>
+                  <BootstrapForm.Control
+                    as="textarea"
+                    name="description"
+                    value={values.description}
+                    onChange={handleChange}
+                    onBlur={handleBlur}
+                    isInvalid={touched.description && !!errors.description}
+                  />
+                  <BootstrapForm.Control.Feedback type="invalid">
+                    {errors.description}
+                  </BootstrapForm.Control.Feedback>
+                </BootstrapForm.Group>
+                <Button type="submit" disabled={isSubmitting} className="submit-button">
+                  Add Blog
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
+    </Container>
   );
 }
 
